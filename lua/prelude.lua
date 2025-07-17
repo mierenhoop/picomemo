8c5c41e04c8c006c2ceb8de1da383936ee85d706
mierenhoop <46939409+mierenhoop@users.noreply.github.com>
2025-07-17 17:03:27 +0200

    Remove non-lomemo lua code
    Preparation for merge. Code is moved to another repository as it's not
    relevant to picomemo.

--- lua/prelude.lua
+++
@@ -1,5 +0,0 @@
--- error when assigning or indexing global
-setmetatable(_G, {
-  __index = function(self,k) error("trying to index global '"..k.."'") end,
-  __newindex = function(self,k) error("trying to assign global '"..k.."'") end,
-})
