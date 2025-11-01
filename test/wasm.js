0bc4078347ef73570248098a854c211501efbe88
mierenhoop <46939409+mierenhoop@users.noreply.github.com>
2025-11-01 21:38:27 +0100

    Get session working in JS

--- test/wasm.js
+++ test/wasm.js
@@ -1,36 +1,147 @@
 let assert = require('node:assert');
 let omemo = require("../o/omemo.js")
 
-
-function handleRet(r) {
-  if (r != 0) throw "omemo error: " + r
-}
-
-class Store {
-  constructor() {
-    this.store = omemo._AllocateStore()
-    if (this.store == 0) throw "malloc fail"
-  }
-
-  setup() { handleRet(omemo._omemoSetupStore(this.store)) }
-
-  free() { omemo._free(this.store) }
-}
-
-class Session {
-  constructor() {
-    this.session = omemo._AllocateSession()
-    if (this.session == 0) throw "malloc fail"
-  }
-
-  free() { omemo._free(this.session) }
-}
-
 function malloc(n) {
   let p = omemo._malloc(n)
   if (p == 0) throw "malloc fail"
   console.log("Alloced: " + n)
   return p
+}
+
+function calloc(n) {
+  let p = malloc(n)
+  omemo.HEAPU8.fill(0, p, p+n)
+  return p
+}
+
+function handleRet(r) {
+  if (r != 0) throw "omemo error: " + r
+}
+
+function getSlice(p, n) {
+  return omemo.HEAPU8.slice(p, p+n)
+}
+
+function toSerKey(p) {
+  // We can do this because p > 0 as null pointer is invalid
+  let b = getSlice(p-1, 33)
+  b[0] = 5
+  return b
+}
+
+class Store {
+  constructor() {
+    this.store = calloc(omemo._get_storesize())
+  }
+
+  setup() { handleRet(omemo._omemoSetupStore(this.store)) }
+
+  deserialize(ser) {
+    malloced({
+      s: ser
+    }, ({s}) => {
+      handleRet(omemo._omemoDeserializeStore(s, ser.length, this.store))
+    })
+  }
+
+  serialize() {
+    return malloced({
+      d: omemo._omemoGetSerializedStoreSize(this.store)
+    }, ({d}, resolve) => {
+      omemo._omemoSerializeStore(d, this.store)
+      return resolve(d)
+    })
+  }
+
+  getBundle() {
+    let n = omemo._get_numprekeys()
+    let prekeys = []
+    for (let i = 0; i < n; i++) {
+      let id = omemo._get_store_pk_id(this.store, i)
+      if (id != 0) prekeys.push({
+        id,
+        key: toSerKey(omemo._get_store_pk(this.store, i)),
+      })
+    }
+    return {
+      ik:   toSerKey(omemo._get_store_ik(this.store)),
+      spk:  toSerKey(omemo._get_store_spk(this.store)),
+      spks: getSlice(omemo._get_store_spks(this.store), 64),
+      spk_id: omemo._get_store_spk_id(this.store),
+      prekeys,
+    }
+  }
+
+  free() { omemo._free(this.store) }
+}
+
+// TODO: store in session?
+class Session {
+  constructor() {
+    this.session = calloc(omemo._get_sessionsize())
+  }
+
+  deserialize(ser) {
+    malloced({
+      s: ser
+    }, ({s}) => {
+      handleRet(omemo._omemoDeserializeSession(s, ser.length, this.session))
+    })
+  }
+
+  serialize() {
+    return malloced({
+      d: omemo._omemoGetSerializedSessionSize(this.session)
+    }, ({d}, resolve) => {
+      omemo._omemoSerializeSession(d, this.session)
+      return resolve(d)
+    })
+  }
+
+  initiateSession(store, {
+    spks, spk, ik, pk, spk_id, pk_id,
+  }) {
+    malloced({spks,spk,ik,pk},({spks,spk,ik,pk})=> {
+      handleRet(omemo._omemoInitiateSession(this.session, store.store,
+        spks, spk, ik, pk, spk_id, pk_id
+      ))
+    })
+  }
+
+  encryptKey(store, key) {
+    return malloced({
+      msg: omemo._get_keymessagesize(),
+      k: key,
+    }, ({msg,k}) => {
+      handleRet(omemo._omemoEncryptKey(this.session, store.store, msg, k, key.length))
+      return {
+        msg: getSlice(omemo._get_km_p(msg),
+                      omemo._get_km_n(msg)),
+        prekey: omemo._get_km_ispk(msg) != 0,
+      }
+    })
+  }
+
+  decryptKey(store, prekey, msg) {
+    // for oldmemo
+    let keysize = 32
+    return malloced({
+      msgp: msg,
+      key: keysize,
+      // Reserve 8 bytes for out pointer in case sizeof size_t == 8
+      // It's little endian so it's no problem either way (4 vs 8)
+      keysizep: 8,
+    }, ({msgp, key, keysizep}, resolve) => {
+      omemo.HEAPU8[keysizep] = keysize
+      handleRet(omemo._omemoDecryptKey(this.session, store.store, key, keysizep, prekey ? 1 : 0, msgp, msg.length))
+      // TODO: do something with new keysize
+      return resolve(key)
+    })
+  }
+
+  // You probaly don't have to free a Session or Store at all because in the
+  // intended usecase you rarely destroy sessions anyways
+  free() { omemo._free(this.session) }
 }
 
 function toBuf(b) {
@@ -52,6 +163,7 @@ function malloced(m, cb) {
   let pton = {}
   let resolve = p => omemo.HEAPU8.slice(p,p+pton[p])
   try {
+    // TODO: we can put all in one malloc
     for (let [k,v] of Object.entries(m)) {
       let [p,n] = (typeof v == "number") ?
         [malloc(v),v] : [toHeap(v),v.length]
@@ -104,22 +216,35 @@ function decryptMessage(key, iv, payload) {
   })
 }
 
+function getRandomPrekey(prekeys) {
+  return prekeys.length > 0 ?
+    prekeys[(Math.random()*prekeys.length) | 0] : null
+}
 
 omemo.onRuntimeInitialized = () => {
-  let store = omemo._AllocateStore()
-  assert.equal(omemo._omemoSetupStore(store), 0)
-  let len = omemo._omemoGetSerializedStoreSize(store)
-  let ser = omemo._malloc(len)
-  omemo._omemoSerializeStore(ser, store)
-  omemo._free(store)
-  let store2 = omemo._AllocateStore()
-  assert.equal(omemo._omemoDeserializeStore(ser, len, store2), 0)
-  omemo._free(ser)
-  omemo._free(store2)
+  let store1 = new Store()
+  let store2 = new Store()
+  store1.setup()
+  store2.setup()
 
+  let session1 = new Session()
+  let session2 = new Session()
+
+  let bundle = store1.getBundle()
+  let pk = getRandomPrekey(bundle.prekeys)
+  delete bundle.prekeys
+  bundle.pk = pk.key
+  bundle.pk_id = pk.id
+
+  session2.initiateSession(store2, bundle)
+
+  let plain = "Hello"
   let enc = encryptMessage(toBuf("Hello"))
-  console.log(enc)
-  console.log(new TextDecoder().decode(decryptMessage(enc.key, enc.iv, enc.payload)))
+  let {prekey, msg} = session2.encryptKey(store2, enc.key)
+  let key = session1.decryptKey(store1, prekey, msg)
+  //store.free()
+
+  assert.equal(new TextDecoder().decode(decryptMessage(key, enc.iv, enc.payload)), plain)
 
   console.log("Test successful")
 }
--- wasm.c
+++ wasm.c
@@ -8,14 +8,31 @@
 #include "omemo.c"
 #include "hacl.c"
 
+#define EX EMSCRIPTEN_KEEPALIVE
+
 // rm -f o/test-omemo* && CFLAGS="-Os -s -flto -DOMEMO2 -s EXPORTED_FUNCTIONS=_malloc,_main" MBED_VENDOR=mbedtls emmake make o/test-omemo && ls -lah o/test-omemo.wasm
 //
 // TODO: implement En/DecryptMessage in js
 
-extern int omemoJsLoadMessageKey(int,int,int,int);
-extern int omemoJsStoreMessageKey(int,int,int,int,int);
+//extern int omemoJsLoadMessageKey(int,int,int,int);
+//extern int omemoJsStoreMessageKey(int,int,int,int,int);
 extern void omemoJsRandom(int,int);
 
+// When omemoStoreMessageKey is called and !g_skipbuf then g_nskip will
+// contain amount of to-be-stored keys and omemoDecryptKey will return
+// with user error. Now the JS caller will allocate and set g_skipbuf
+// with enough space to hold g_nskip keys.
+static uint64_t g_nskip;
+static struct omemoMessageKey *g_skipbuf;
+
+// When omemoLoadMessageKey is called g_loadkey is set to the key arg
+// and return with user error. The JS caller will put the mk into
+// g_loadkey and call omemoDecryptKey again.
+// TODO: what if key not found??
+static bool g_suppliedkey;
+static struct omemoMessageKey g_loadkey;
+
+// TODO: simplify, just only have extern??
 int omemoRandom(void *p, size_t n) {
   omemoJsRandom((int)p, (int)n);
   return 0;
@@ -24,22 +41,54 @@ int omemoRandom(void *p, size_t n) {
 // TODO: don't need EMSCRIPTEN_KEEPALIVE for these two, we should probably say something like OMEMO_USER instead of OMEMO_EXPORT for these
 int omemoLoadMessageKey(struct omemoSession *s,
                                      struct omemoMessageKey *k) {
-  return omemoJsLoadMessageKey(k->nr, (int)k->dh, (int)k->mk, (int)s);
+  if (g_suppliedkey) {
+    memcpy(g_loadkey.mk, k->mk, 32);
+    g_suppliedkey = false;
+    return 0;
+  } else {
+    g_loadkey.nr = k->nr;
+    memcpy(g_loadkey.dh, k->dh, 32);
+    return 1;
+  }
+  //return omemoJsLoadMessageKey(k->nr, (int)k->dh, (int)k->mk, (int)s);
 }
 
 int omemoStoreMessageKey(struct omemoSession *s,
                                       const struct omemoMessageKey *k,
                                       uint64_t n) {
-  return omemoJsStoreMessageKey(k->nr, (int)k->dh, (int)k->mk, n, (int)s);
+  if (g_skipbuf) {
+    memcpy(k, g_skipbuf+(g_nskip - n), sizeof(*k));
+    return 0;
+  }
+  return OMEMO_EUSER-1;
 }
 
-EMSCRIPTEN_KEEPALIVE
-struct omemoSession *AllocateSession(void) {
-  return calloc(1, sizeof(struct omemoSession));
-}
+EX int get_sessionsize   (void) { return sizeof(struct omemoSession   ); }
+EX int get_storesize     (void) { return sizeof(struct omemoStore     ); }
+EX int get_messagekeysize(void) { return sizeof(struct omemoMessageKey); }
+EX int get_keymessagesize(void) { return sizeof(struct omemoKeyMessage); }
+EX int get_numprekeys    (void) { return OMEMO_NUMPREKEYS              ; }
 
-EMSCRIPTEN_KEEPALIVE
-struct omemoStore *AllocateStore(void) {
-  return calloc(1, sizeof(struct omemoStore));
-}
+EX uint8_t *get_mk_dh(struct omemoMessageKey *mk) { return mk->dh; }
+EX uint32_t get_mk_nr(struct omemoMessageKey *mk) { return mk->nr; }
+// For heartbeat
+EX uint32_t get_session_nr(struct omemoSession *s) { return s->state.nr; }
 
+EX uint8_t *get_km_p   (struct omemoKeyMessage *m) { return m->p       ; }
+EX size_t   get_km_n   (struct omemoKeyMessage *m) { return m->n       ; }
+EX bool     get_km_ispk(struct omemoKeyMessage *m) { return m->isprekey; }
+
+EX uint8_t *get_store_ik    (struct omemoStore *s) { return s->identity.pub          ; }
+EX uint8_t *get_store_spk   (struct omemoStore *s) { return s->cursignedprekey.kp.pub; }
+EX uint8_t *get_store_spks  (struct omemoStore *s) { return s->cursignedprekey.sig   ; }
+EX uint32_t get_store_spk_id(struct omemoStore *s) { return s->cursignedprekey.id    ; }
+
+EX uint32_t get_store_pk_id(struct omemoStore *s, int i) { return s->prekeys[i].id; }
+EX uint8_t *get_store_pk   (struct omemoStore *s, int i) { return s->prekeys[i].kp.pub; }
+
+EX
+void FillMessageKey(struct omemoMessageKey *k, uint32_t nr, omemoKey dh, omemoKey mk) {
+  k->nr = nr;
+  memcpy(k->dh, dh, 32);
+  memcpy(k->mk, mk, 32);
+}
